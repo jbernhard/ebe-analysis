2123e5dffd0d46c925f48bdaf4189616b40afc25
Jonah Bernhard <jeb65@grads-ad>
2013-07-01 13:55:18 -0400

    Add particle filter class.

--- lib/particle.py
+++ lib/particle.py
@@ -66,3 +66,78 @@ class Particle:
     def _calc_eta(self,px,py,pz,sqrt=math.sqrt,log=math.log):
         pmag = sqrt(px*px + py*py + pz*pz)
         return 0.5*log((pmag+pz)/max(pmag-pz,1e-10))   # avoid division by zero
+
+
+
+class ParticleFilter:
+    """
+    Creates a function to filter Particles with specified criteria.  The
+    function returns True if all criteria are met, else False.
+
+    Usage
+    -----
+    >>> pf = ParticleFilter(criterion1,criterion2,...)
+
+    Allowed criteria are:
+    IDlist -- a list of particle IDs
+    charged -- boolean, shortcut to select all charged particles
+    pTmin,pTmax -- range of pT; can omit either min or max
+    etamin,etamax -- range of eta; if only etamax, assume |eta| < etamax;
+        if only etamin, assume etamin < |eta|
+
+    After creating a ParticleFilter, it is then applied to Particle objects,
+    e.g.
+
+    >>> pf = ParticleFilter(IDlist=[211],pTmin=0.5,etamax=2.5)
+    >>> p1 = Particle(ID=211,pT=1.0,phi=1.0,eta=1.0)
+    >>> pf(p1)
+    True
+    >>> p2 = Particle(ID=211,pT=1.0,phi=1.0,eta=3.0)
+    >>> pf(p2)
+    False
+
+    """
+
+    def __init__(self,IDlist=[],charged=False,pTmin=None,pTmax=None,etamin=None,etamax=None):
+        # init. empty list of filters
+        self._filters = []
+
+        ### create a lambda function for all specified criteria
+        ### each lambda is a boolean function of Particle objects
+
+        # match particle ID
+        if IDlist:
+            self._filters.append(lambda Particle: Particle.ID in IDlist)
+
+        # match charged particles
+        if charged:
+            # need to implement this
+            #self._filters.append(lambda Particle: Particle.ID in ...)
+            pass
+
+
+        # match pT range
+        if pTmin and pTmax:
+            self._filters.append(lambda Particle: pTmin < Particle.pT < pTmax)
+
+        elif pTmin and not pTmax:
+            self._filters.append(lambda Particle: pTmin < Particle.pT)
+
+        elif pTmax and not pTmin:
+            self._filters.append(lambda Particle: Particle.pT < pTmax)
+
+
+        # match eta range
+        if etamax and etamin:
+            self._filters.append(lambda Particle: etamin < Particle.eta < etamax)
+
+        elif etamin and not etamax:
+            self._filters.append(lambda Particle: etamin < abs(Particle.eta))
+
+        elif etamax and not etamin:
+            self._filters.append(lambda Particle: abs(Particle.eta) < etamax)
+
+
+    def __call__(self,Particle):
+        # match all filter functions
+        return all(f(Particle) for f in self._filters)
