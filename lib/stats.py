c62fe9d8b4388aa07cec82dfbbd0b7d3af9e1ee1
Jonah Bernhard <jeb65@grads-ad>
2013-10-10 14:37:42 -0400

    Major overhaul of RawData class.

--- lib/stats.py
+++ lib/stats.py
@@ -3,107 +3,161 @@ Statistics.
 """
 
 
-from itertools import starmap
-
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.random import uniform
-from scipy.stats import gengamma
+from scipy.stats import gengamma, norm
 from scipy.optimize import curve_fit
 
 
-def gengamma_start(data=None):
+
+"""
+Plot style settings.
+
+"""
+
+_figwidth = 10
+
+plt.rc('figure', figsize=[_figwidth,_figwidth/1.618], facecolor='1.0')
+plt.rc('font', family='serif')
+plt.rc('axes', color_cycle=['#33b5e5','#99cc00','#ff4444','#aa66cc','#ffbb33'])
+plt.rc('lines', linewidth=1.5)
+plt.rc('patch', linewidth=1.5)
+
+
+
+def rms(x,y=None):
     """
-    Generate random starting parameters for fitting a generalized gamma
-    distribution.  Randomization is necessary to avoid systematic convergence
-    problems.
-
-    Parameter ranges are sensible for v_n distributions.
-
-    Returns
-    -------
-    (a, c, loc, scale) -- where a,c are shape params.
-
-    """
-
-    return tuple(starmap(uniform, ((0.1,2.0),(1.0,5.0),(-0.1,0.1),(0.01,0.3))))
-
-gengamma._fitstart = gengamma_start
-
-
-def fit_file(fname,dist='gengamma',**kwargs):
-    """
-    Fit a distribution to each column of a data file.
+    Calculate the root mean square of a data set.
 
     Arguments
     ---------
-    fname -- file object, filename, or generator containing data columns to fit
-    dist -- distribution to fit, either 'gengamma' (default) or 'norm'
-    kwargs -- for np.loadtxt, except 'unpack' or 'ndmin' are ignored
+    x -- array-like, required
+    y -- array-like, optional
+
+    If y is ommitted, the RMS of x is calculated.  If both x and y are
+    specified, x is interpreted as bin locations and y as the corresponding bin
+    values.
 
     Returns
     -------
-    iterable of MLE parameters:
-
-        params_0, ... , params_N
-
-    for each column, where params are tuples of the form
-
-        (*shapes, loc, scale)
-
-    as produced by scipy.stats.rv_continuous.fit
+    rms -- float
 
     """
 
-    # ignore divide by zero errors
-    # happens when the pdf vanishes because log pdf is used for optimization
-    # doesn't affect the final result
-    np.seterr(divide='ignore')
+    x = np.asarray(x)
 
-    # remove 'unpack' and 'ndmin' kwargs if set
-    for key in ['unpack','ndmin']:
-        try:
-            del kwargs[key]
-        except KeyError:
-            pass
-
-    # read file
-    cols = np.loadtxt(fname,unpack=True,ndmin=2,**kwargs)
-
-    # set fitting distribution
-    if dist == 'norm':
-        # fitting a gaussian is equivalent to calculating mean and stddev.
-        # but the latter is much faster
-        _fit = describe
-
+    if y is None:
+        return np.sqrt(np.mean(np.square(x)))
     else:
+        y = np.asarray(y)
+        return np.sqrt( np.sum(y*np.square(x)) / np.sum(y) )
+
+
+"""
+Starting parameters for fitting the generalized gamma distribution.
+
+Flow distributions seem to consistently have rms ~ scale.
+
+order:  a, c, loc, scale -- where a,c are shape params.
+
+"""
+
+gengamma._fitstart = lambda *args: (1., 2., 0., rms(*args))
+
+
+class RawData:
+    """
+    Store raw (unbinned) data and provide related methods.
+
+    Arguments
+    ---------
+    data -- array-like, will be flattened
+    dist -- name of scipy distribution which is expected to describe the data
+    maxstd -- maximum allowed standard deviations from the mean;
+              points further away are removed
+
+    """
+
+    def __init__(self,data,dist='gengamma',maxstd=10):
+        # validate the distribution
         try:
             dist = eval(dist)
         except NameError:
             raise ValueError('invalid distribution: ' + dist)
         else:
-            _fit = dist.fit
+            self.dist = dist
 
-    return map(_fit, cols)
+        # flatten
+        data = np.ravel(data)
+
+        # remove outliers and store
+        self.data = data[np.abs(data - data.mean()) < maxstd*data.std()]
 
 
-def describe(data):
-    """
-    Calculate descriptive stats (mean, stddev).
+    @classmethod
+    def from_table(cls,data,dist='gengamma',**kwargs):
+        """
+        Create several RawData instances from the columns of tabular data.  Each
+        column is expected to correspond to a separate data set.
 
-    Arguments
-    ---------
-    data -- array-like
+        Arguments
+        ---------
+        data -- array-like, file object, filename, or generator containing
+                tabular data
+        dist -- same as for __init__
+        kwargs -- passed to np.loadtxt if reading a file
 
-    Returns
-    -------
-    (mean, stddev)
+        Returns
+        -------
+        iterable of RawData instances for each column
 
-    """
+        """
 
-    data = np.asarray(data)
+        if any(isinstance(data,t) for t in (np.ndarray,list,tuple)):
+            data = np.asarray(data).T
+        else:
+            kwargs.update(unpack=True)
+            data = np.loadtxt(data,**kwargs)
 
-    return (data.mean(), data.std())
+        data = np.atleast_2d(data)
+
+        return (cls(col,dist=dist) for col in data)
+
+
+    def describe(self):
+        """ Calculate mean and standard deviation. """
+
+        return self.data.mean(), self.data.std()
+
+
+    def fit(self):
+        """
+        Calculate MLE distribution parameters.
+
+        Returns
+        -------
+        *shapes, loc, scale -- as produced by scipy.stats.rv_continuous.fit
+
+        """
+
+        if self.dist is norm:
+            return self.describe()
+        else:
+            return self.dist.fit(self.data)
+
+
+    def plot(self):
+        """ Fit and plot the data. """
+
+        x = np.linspace(self.data.min(),self.data.max(),100)
+
+        params = self.fit()
+
+        plt.plot(x, self.dist.pdf(x, *params))
+        plt.hist(self.data, bins=int(2*self.data.size**.33),
+                histtype='step', normed=True)
+
+        plt.show()
 
 
 
